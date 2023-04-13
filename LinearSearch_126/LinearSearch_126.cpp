7a66a292f9c741c7c1b6ae6a3a680b81d3a54ca9
mutiara126 <salsamutiara1812@gmail.com>
2023-04-13 15:28:37 +0800

    Add project files.

---
+++ LinearSearch_126/LinearSearch_126.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namwespaces std;
+
+int arr[20];       //Array to be searched
+int n;            //Number of element
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
