472586f0dbe922d941e7162d6ea4e73aa8bfb37e
mutiara126 <salsamutiara1812@gmail.com>
2023-04-13 15:45:20 +0800

    menerima element array

--- LinearSearch_126/LinearSearch_126.cpp
+++ LinearSearch_126/LinearSearch_126.cpp
@@ -5,7 +5,6 @@ int arr[20];       //Array to be searched
 int n;            //Number of element
 int i;            //Index of array element
 
-
 void input()
 {
     while (true)
@@ -17,6 +16,15 @@ void input()
         else
             cout << "\nArray should have minumum 1 and maximum 20 elements. \n\n";
     }
+   
+    //Accept array elements
+    cout << "\n----------------\n";
+    cout << " Enter array elements \n";
+    cout << "----------------------\n";
+    for (i = 0; 1 < n; 1++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
 }
-
 
